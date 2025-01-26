da6505aa264794015e9074d36299073dbf199f77
riso34 <sou4.0304@gmail.com>
2025-01-26 21:12:34 +0900

    ナビゲーションドロワー機能追加

---
+++ app/src/main/java/com/example/calendar/CalendarView.kt
@@ -0,0 +1,123 @@
+package com.example.calendar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import timber.log.Timber
+import java.time.DayOfWeek
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
+
+@Composable
+fun CalendarCompose() {
+    val currentMonth = remember { YearMonth.now() }
+    val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
+    val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
+    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
+    var selection by remember { mutableStateOf<CalendarDay?>(null) }
+
+    val state = rememberCalendarState(
+        startMonth = startMonth,
+        endMonth = endMonth,
+        firstVisibleMonth = currentMonth,
+        firstDayOfWeek = firstDayOfWeek
+    )
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalCalendar(
+            state = state,
+            monthHeader = { month ->
+                val dayOfWeek = month.weekDays.first().map { it.date.dayOfWeek }
+                MonthHeader(daysOfWeek = dayOfWeek)
+            },
+            dayContent = {
+                Day(it, isSelected = selection == it) { clickDay ->
+                    selection = clickDay
+                    Timber.d("clickDay: $clickDay")
+                }
+            },
+            monthBody = { _, content ->
+                Box(
+                    modifier = Modifier.background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                                MaterialTheme.colorScheme.tertiary.copy(alpha = 0.2f)
+                            )
+                        )
+                    )
+                ) {
+                    content()
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun Day(day: CalendarDay, isSelected: Boolean = false, onClick: (CalendarDay) -> Unit = {}) {
+    val textColor = when (day.position) {
+        DayPosition.MonthDate -> Color.Unspecified
+        DayPosition.InDate, DayPosition.OutDate -> Color.LightGray
+    }
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f)    // This is important for square sizing!
+            .background(color = if (isSelected) Color.Cyan else Color.Transparent)
+            .clickable(enabled = day.position == DayPosition.MonthDate){onClick(day)},
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = day.date.dayOfMonth.toString(), color=textColor)
+    }
+}
+
+fun DayOfWeek.displayText(uppercase: Boolean = false): String {
+    return getDisplayName(TextStyle.SHORT, Locale.getDefault()).let { value ->
+        if (uppercase) value.uppercase(Locale.getDefault()) else value
+    }
+}
+
+@Composable
+private fun MonthHeader(daysOfWeek: List<DayOfWeek>) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .testTag("MonthHeader"),
+    ) {
+        for (dayOfWeek in daysOfWeek) {
+            Text(
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                fontSize = 15.sp,
+                text = dayOfWeek.displayText(),
+                fontWeight = FontWeight.Medium,
+            )
+        }
+    }
+}
